c8cf58edc858dcc0eaad8c7a0ab469e481a8d86b
Vilmos Ioo <cool.villi@gmail.com>
2013-12-23 23:56:01 +0000

    Support for demo and github url.

--- app/single-project.php
+++ app/single-project.php
@@ -10,7 +10,16 @@ get_header();
 			<h1><?php the_title(); ?></h1>
 		</header>
 		<?php the_content(); ?>
-		<?php if(get_the_title() == 'Tumbleblog') get_template_part('tumbleblog'); ?>
+		<?php 
+			$custom_fields = get_post_custom();
+			$url = $custom_fields['url'];
+			$demo = $custom_fields['demo-link'];
+			echo '<div class="tcenter">';
+			if(!empty($url) && !empty($url[0])) echo "<a href='$url[0]' class='button large blue'>View on github</a>";
+			if(!empty($demo) && !empty($demo[0])) echo "<a href='$demo[0]' class='button large orange'>Demo</a>";
+			echo '</div>';
+		?>
+		<?php if(get_the_title() == 'Tumbleblog') get_template_part('tumbleblog'); ?>		
 	<?php endwhile; ?>
 </section>
 <?php get_footer(); ?>
