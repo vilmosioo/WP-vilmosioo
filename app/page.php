ea88c4d79fca4479b2bd54e889a23f76fd23ca19
Vilmos Ioo <cool.villi@gmail.com>
2013-12-28 18:52:02 +0000

    Added content.php for DRY code.

--- app/page.php
+++ app/page.php
@@ -6,10 +6,7 @@ get_header();
 ?>
 <section class='content full clearfix'>
 	<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
-		<header>
-			<h1><?php the_title(); ?></h1>
-		</header>
-		<?php the_content();?>
+		<?php get_template_part( 'content', get_post_format() ); ?>
 	<?php endwhile; ?>
 </section>
 <?php get_footer(); ?>
