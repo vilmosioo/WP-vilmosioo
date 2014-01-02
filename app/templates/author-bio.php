2bce188030294876915f9e2e651b0e417e1285fd
Vilmos Ioo <cool.villi@gmail.com>
2014-01-02 00:03:17 +0000

    Updated author bio

--- app/templates/author-bio.php
+++ app/templates/author-bio.php
@@ -8,7 +8,10 @@ Template to display author biography.
 	<h4> About <strong><?php the_author_meta( 'display_name' ); ?></strong> </h4>
 	<p> 
 		<?php the_author_meta( 'description' ); ?>
-		<a href='#' rel='canonical'>Find out more &rarr;</a> 
+		<?php 
+			$page = get_page_by_title( 'About' );
+			echo "<a href='".get_permalink( $page->ID )."' rel='canonical'>Find out more &rarr;</a>";
+		?> 
 	</p>
 	<div class='clear'></div>
 </aside>
