8b9b74a963f264d43eb2fdd115351502fe086b39
Vilmos Ioo <cool.villi@gmail.com>
2014-01-06 00:55:04 +0000

    Added class to tumblelog.

--- dist/templates/post-features.php
+++ dist/templates/post-features.php
@@ -6,7 +6,8 @@ Template to print the post features
 <h3>What I did</h3>
 <ol class='rectangle-list'>
 <?php
-	foreach(get_the_terms(get_the_ID(), get_post_type().'-features') as $feature){
+	$features = get_the_terms(get_the_ID(), get_post_type().'-features');
+	if(is_array($features)) foreach($features as $feature){
 		echo "<li><a>".$feature->name."</a></li>";
 	}
 ?>
