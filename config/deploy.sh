accfc87e9069e529454b05e4aca1e32e30cea95d
Vilmos Ioo <vilmosi@blinkbox.com>
2014-08-14 11:02:12 +0100

    Changed order of deploy

--- config/deploy.sh
+++ config/deploy.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
+echo 'Starting deployment to server...'
+grunt ftp-deploy || { echo 'FTP deployment failed.' ; exit 1; }
+
 echo 'Pushing git data to repo...'
 git push origin master || { echo 'Git push failed.' ; exit 1; }
 git push origin master --tags || { echo 'Git tagging failed.' ; exit 1; }
-
-echo 'Starting deployment to server...'
-grunt ftp-deploy || { echo 'FTP deployment failed.' ; exit 1; }
 
 echo 'SUCCESS!'
 exit 0
