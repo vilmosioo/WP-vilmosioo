3f7ecc0c4a3ede599e3d11aab4716c311c359404
Vilmos Ioo <cool.villi@gmail.com>
2017-02-24 21:35:22 +0000

    Added build for travis

--- config/build.sh
+++ config/build.sh
@@ -1,13 +1,5 @@
 #!/bin/sh
 
-echo 'Switching to master and setting identity for git'
-git checkout master
-git config user.email 'ioo.vilmos@gmail.com' || { echo 'Git auth failed' ; exit 1; }
-git config user.name 'Vilmos Ioo'
-
-echo 'Patching version...'
-npm version patch -m "Updating version --skip-ci" || { echo 'Version patch failed' ; exit 1; }
-
 echo 'Test and build artefacts...'
 grunt || { echo 'Grunt build failed' ; exit 1; }
 
